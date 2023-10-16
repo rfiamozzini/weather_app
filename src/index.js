b52a9ffa5f238b6bdddf6d19584b99808b540937
rfiamozzini <beca.0205@gmail.com>
2023-10-16 15:17:48 +0200

    Forecast / API

--- src/index.js
+++ src/index.js
@@ -43,6 +43,13 @@ let apiKey = "d2dea6f10ea74e3ef22300c3ed13b2a2";
 
 // Homework Week 5
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function searchCity(response) {
   console.log(response.data.main.temp_max);
 
@@ -80,6 +87,8 @@ function searchCity(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function searchAndDisplay(event) {
@@ -139,7 +148,7 @@ fahrenheitLink.addEventListener("click", showFahrenheit);
 
 //forecast
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -165,5 +174,3 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-displayForecast();
